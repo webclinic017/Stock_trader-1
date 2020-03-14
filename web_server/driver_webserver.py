3072645d3e1b482352937ee1199681ca56675a62
Jonathan Kelly <jonathankelly@hotmail.ca>
2020-03-14 13:00:36 -0700

    docker-compose web server debug

--- web_server/driver_webserver.py
+++ web_server/driver_webserver.py
@@ -1,22 +1,28 @@
 import socket
 import json
+import os
 import multiprocessing
+
 BUFFER_SIZE = 4096
 protocol = "http"
 server_name = "web server"
 
 # transaction_server_ip = "192.168.1.229"  # IP on comp 17
-transaction_server_ip = audit_log_server_ip = "localhost"  # IP on home comp
-transaction_server_port = 44415
-audit_log_server_port = 44416
-static_server_host = "localhost"
-static_server_port = 44420
-web_server_host = "localhost"
-web_server_port = 44419
+transaction_server_ip = os.environ['TRANS_HOST']
+audit_log_server_ip = os.environ['LOG_HOST']  # IP on home comp
+transaction_server_port = os.environ['TRANS_PORT']
+audit_log_server_port = os.environ['LOG_HOST']
+# static_server_host = os.environ['MY_HOST']
+# static_server_port = os.environ['MY_HOST']
+web_server_host = os.environ['MY_HOST']
+web_server_port = os.environ['MY_PORT']
 base_url = f"{web_server_host}:{web_server_port}"
 
-def send_to_trans_server(transaction_payload):
+print(str(os.environ['MY_HOST']))
+print(web_server_port)
 
+
+def send_to_trans_server(transaction_payload):
     sckt_trans = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sckt_trans.connect((transaction_server_ip, transaction_server_port))
 
@@ -30,18 +36,21 @@ def send_to_trans_server(transaction_payload):
 
     return str.encode(trans_response)
 
+
 def is_index_route(http_request):
     first_line = http_request.split("\n")[0]
     endpoint = first_line.split(" ")[1]
     result = endpoint == "/"
     return result
 
+
 def get_transaction_payload(http_request):
     return http_request.split("\n")[-1]
 
+
 def listen():
     web_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    web_socket.bind((web_server_host, web_server_port))
+    web_socket.bind((web_server_host, int(web_server_port)))
     print("listening on " + str(web_server_host) + ":" + str(web_server_port))
     web_socket.listen(10)
     while (True):
@@ -68,10 +77,12 @@ def listen():
             print(type(e))
             print(e)
 
+
 def main_page():
     print("index route invoked")
     return str.encode("landing page stub")
-    #return render_template("day_trader.html")
+    # return render_template("day_trader.html")
+
 
 if __name__ == "__main__":
     listen()
